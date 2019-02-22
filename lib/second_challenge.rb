5a1bf14eb246ddfad3054048bb0040a1708a4cf6
Mickae Truman <mtruman@nyit.edu>
2019-02-22 01:16:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {[:patriarch] = {""}, :matriarch, :hero, :hero_friends
+    :montague => {[:patriarch] = "{}", :matriarch, :hero, :hero_friends
       #your key/value pairs here!
       }, 
     :capulet => {:patriarch, :matriarch, :heroine, :heroine_friends
